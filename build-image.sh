31e36d4ed25208ba0eb109d662112d8af0b0ed3c
Thanh Hoang <hoangthanh751996@gmail.com>
2019-05-22 14:37:52 +0700

    upate

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,4 @@
-docker build -t hoangthanh751996/test-docker .
-docker push hoangthanh751996/test-docker
+docker build -t hoangthanh751996/test-docker:1.0 .
+docker push hoangthanh751996/test-docker:1.0
+docker rmi hoangthanh751996/test-docker:1.0
 
